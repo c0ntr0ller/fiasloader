16248117383643f972a539fd9632604354b12cda
bek <bek.andrey@gmail.com>
2018-10-03 18:35:43 +0700

    -tune

--- src/main/java/ru/progmatik/main/config/FiasConfiguration.java
+++ src/main/java/ru/progmatik/main/config/FiasConfiguration.java
@@ -1,10 +1,19 @@
 package ru.progmatik.main.config;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 import ru.progmatik.main.webclient.FiasClient;
+
+import javax.net.ssl.SSLContext;
 
 /**
  * класс конфигурации для бинов маршаллинга
@@ -12,24 +21,55 @@ import ru.progmatik.main.webclient.FiasClient;
 @Configuration
 public class FiasConfiguration {
 
-    @Value("${fiasurl}")
+    @Value("${client.default-uri}")
     private String fiasurl;
 
+    @Value("${client.ssl.trust-store}")
+    private Resource trustStore;
+
+    @Value("${client.ssl.trust-store-password}")
+    private String trustStorePassword;
+
     @Bean
-    public Jaxb2Marshaller marshaller(){
+    public Jaxb2Marshaller jaxb2Marshaller(){
         Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
         jaxb2Marshaller.setContextPath("fias.wsdl");
         return jaxb2Marshaller;
     }
 
     @Bean
-    public FiasClient fiasClient(Jaxb2Marshaller marshaller){
+    public FiasClient fiasClient(Jaxb2Marshaller marshaller) throws Exception {
         FiasClient client = new FiasClient();
 
         client.setDefaultUri(fiasurl);
         client.setUnmarshaller(marshaller);
         client.setMarshaller(marshaller);
+        client.setMessageSender(httpComponentsMessageSender());
 
         return client;
+    }
+
+    @Bean
+    public HttpComponentsMessageSender httpComponentsMessageSender() throws Exception {
+        HttpComponentsMessageSender httpComponentsMessageSender = new HttpComponentsMessageSender();
+        httpComponentsMessageSender.setHttpClient(httpClient());
+
+        return httpComponentsMessageSender;
+    }
+
+    public HttpClient httpClient() throws Exception {
+        return HttpClientBuilder.create().setSSLSocketFactory(sslConnectionSocketFactory())
+                .addInterceptorFirst(new HttpComponentsMessageSender.RemoveSoapHeadersInterceptor()).build();
+    }
+
+    public SSLConnectionSocketFactory sslConnectionSocketFactory() throws Exception {
+        // NoopHostnameVerifier essentially turns hostname verification off as otherwise following error
+        // is thrown: java.security.cert.CertificateException: No name matching localhost found
+        return new SSLConnectionSocketFactory(sslContext(), NoopHostnameVerifier.INSTANCE);
+    }
+
+    public SSLContext sslContext() throws Exception {
+        return SSLContextBuilder.create()
+                .loadTrustMaterial(trustStore.getFile(), trustStorePassword.toCharArray()).build();
     }
 }
--- src/main/java/ru/progmatik/main/webclient/FiasClient.java
+++ src/main/java/ru/progmatik/main/webclient/FiasClient.java
@@ -6,6 +6,8 @@ import fias.wsdl.GetAllDownloadFileInfoResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
@@ -17,16 +19,18 @@ import java.util.List;
 public class FiasClient extends WebServiceGatewaySupport {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    @Value("${fiasurl}")
+    @Value("${client.default-uri}")
     private String fiasurl;
 
-
     public List<DownloadFileInfo> getAllDownloadFileList(){
+
         GetAllDownloadFileInfo request = new GetAllDownloadFileInfo();
 
         LOGGER.info("Requesting FIAS files list");
 
-        GetAllDownloadFileInfoResponse response = (GetAllDownloadFileInfoResponse) getWebServiceTemplate().
+        final WebServiceTemplate webServiceTemplate = getWebServiceTemplate();
+
+        GetAllDownloadFileInfoResponse response = (GetAllDownloadFileInfoResponse) webServiceTemplate.
                 marshalSendAndReceive(fiasurl, request,
                         new SoapActionCallback(
                                 fiasurl + "/GetAllDownloadFileInfo"));
